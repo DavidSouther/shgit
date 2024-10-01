ce546dd3beb0c42dd6b6c806a55f6cab34509615
David Souther <davidsouther+github@gmail.com>
2024-10-01 14:40:03 +0200

    Allow nested arg calls, like git.remote.add.origin(url)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
+
 from shgit import git
-from pathlib import Path
 
 if __name__ == "__main__":
     git.check = False
@@ -10,4 +11,9 @@ if __name__ == "__main__":
     # git.log("-h")
     git.add(".")
     # git.commit(message="Sh util that uses __getattr__ to choose a bin and **kwargs for flags")
-    git.commit('--amend', message="Sh util that uses __getattr__ to choose a bin and **kwargs for flags")
+    # git.commit('--amend', message="Sh util that uses __getattr__ to choose a bin and **kwargs for flags")
+    git.commit(message="Allow nested arg calls, like git.remote.add.origin(url)")
+    git.remote('-v')
+    # git.remote.add.origin("git@github.com:DavidSouther/shgit.git")
+    # git.push("-u", "origin", "main")
+    git.push()
--- shgit.py
+++ shgit.py
@@ -7,37 +7,42 @@ def make_long_flags(flags: dict[str, str]) -> list[str]:
     return [f"--{key}={val}" for key, val in flags.items()]
 
 
-type StrOrBytesPath = str | bytes | Path
-
-
 class Sh:
-    def __init__(self, prefix=None):
-        self.prefix = prefix
-        self.check = True
-        self.cwd: StrOrBytesPath = Path('.')
-        self.log = False
+    def __init__(self,
+                 prefix: Optional[str | list[str]] = None,
+                 check=True,
+                 cwd: str | bytes | Path = Path("."),
+                 log=False):
+        if prefix is None:
+            self.prefix = []
+        elif isinstance(prefix, str):
+            self.prefix = [prefix]
+        else:
+            self.prefix = prefix
+        self.check = check
+        self.cwd: str | bytes | Path = cwd
+        self.log = log
 
-    def __getattr__(self, name):
-        def sh_call(
-                *args,
-                capture_output=False,
-                check=self.check,
-                cwd: Optional[StrOrBytesPath] = self.cwd,
-                **kwargs: str
-        ):
-            long_args = make_long_flags(kwargs)
-            cmd = [name, *args, *long_args]
-            if self.prefix:
-                cmd = [self.prefix] + cmd
-            if self.log:
-                print(cmd)
-            return run(cmd,
-                       capture_output=capture_output,
-                       check=check,
-                       shell=False,
-                       cwd=cwd)
+    def __call__(self,
+                 *args: str,
+                 capture_output=False,
+                 check=None,
+                 cwd: Optional[str | bytes | Path] = None,
+                 **kwargs: str):
+        check = check or self.check
+        cwd = cwd or self.cwd
+        long_args = make_long_flags(kwargs)
+        cmd: list[str] = [*self.prefix, *args, *long_args]
+        if self.log:
+            print(cmd)
+        return run(cmd,
+                   capture_output=capture_output,
+                   check=check,
+                   shell=False,
+                   cwd=cwd)
 
-        return sh_call
+    def __getattr__(self, name: str):
+        return Sh([*self.prefix, name], self.check, self.cwd, self.log)
 
 
 sh = Sh()
