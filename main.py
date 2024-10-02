d5f9145b6254274ed0dff529a420869398fbb424
David Souther <davidsouther+github@gmail.com>
2024-10-02 18:09:09 +0200

    Pipe as bitwise-or | operator works.

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
-from shgit import git
+from shgit import sh
 
 if __name__ == "__main__":
-    git.check = False
-    git.log = True
-    git.status()
-    # out = git.init(capture_output=True)
-    # print(out)
-    # git.log("-h")
-    git.add(".")
-    # git.commit(message="Sh util that uses __getattr__ to choose a bin and **kwargs for flags")
-    # git.commit('--amend', message="Sh util that uses __getattr__ to choose a bin and **kwargs for flags")
-    # git.commit(message="Allow nested arg calls, like git.remote.add.origin(url)")
-    git.commit(message="Add README and LICENSE.")
-    # git.remote('-v')
-    # git.remote.add.origin("git@github.com:DavidSouther/shgit.git")
-    # git.push("-u", "origin", "main")
-    git.update('--push')
-    git.push()
+    # git.check = False
+    # git.log = True
+    # git.status()
+    # # out = git.init(capture_output=True)
+    # # print(out)
+    # # git.log("-h")
+    # git.add(".")
+    # # git.commit(message="Sh util that uses __getattr__ to choose a bin and **kwargs for flags")
+    # # git.commit('--amend', message="Sh util that uses __getattr__ to choose a bin and **kwargs for flags")
+    # # git.commit(message="Allow nested arg calls, like git.remote.add.origin(url)")
+    # git.commit(message="Add README and LICENSE.")
+    # # git.remote('-v')
+    # # git.remote.add.origin("git@github.com:DavidSouther/shgit.git")
+    # # git.push("-u", "origin", "main")
+    # git.update('--push')
+    # git.push()
+
+    # sh.log = True
+    sh.capture_output = True
+    out = sh.grep('version=', 'setup.py') | sh.awk('-F"', '{print $2}')
+    print(out.stdout, end="")
+
+
--- shgit.py
+++ shgit.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from subprocess import run
-from typing import Optional
+from typing import Any, Optional
 
 
 def make_long_flags(flags: dict[str, str]) -> list[str]:
@@ -38,20 +38,61 @@ class Sh:
         cmd: list[str] = [*self.args, *args, *long_args]
         if self.log:
             print(cmd)
-        return run(cmd,
-                   capture_output=capture_output,
-                   check=check,
-                   shell=False,
-                   cwd=cwd)
+        return Run(cmd, check, cwd, capture_output)
 
     def __getattr__(self, arg: str):
-        return Sh(
-            [*self.args, arg],
-            self.check,
-            self.capture_output,
-            self.cwd,
-            self.log
-            )
+        return Sh([*self.args, arg],
+                  self.check,
+                  self.capture_output,
+                  self.cwd,
+                  self.log)
+
+
+class Run():
+    def __init__(self, args, check, cwd, capture_output):
+        self.args = args
+        self.check = check
+        self.cwd = cwd
+        self.capture_output = capture_output
+        self.pipe = None
+        self.completed = None
+
+    @property
+    def stdout(self):
+        self._run()
+        return self.completed.stdout
+
+    @property
+    def stderr(self):
+        self._run()
+        return self.completed.stderr
+    
+    @property
+    def returncode(self):
+        self._run()
+        return self.completed.returncode
+
+    def check_returncode(self):
+        self._run()
+        return self.completed.check_returncode()
+
+    def _run(self):
+        if self.completed is None:
+            self.completed = run(
+                self.args,
+                capture_output=self.capture_output,
+                check=self.check,
+                shell=False,
+                cwd=self.cwd,
+                input=self.pipe,
+                encoding="utf-8")
+    
+    def __or__(self, other):
+        self.capture_output = True
+        self._run()
+        other.pipe = self.completed.stdout
+        other._run()
+        return other
 
 
 sh = Sh()
